c7dda73cfdb85b6a83c515d01582e13bde8f81c9
David Choi <daaave@gmail.com>
2018-10-25 09:36:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(number){
-  if (number % 3 == 0) do
-    
+def fizzbuzz(int){
+  result = ""
+  if (number % 3 == 0)
+    result << "Fizz"
   end
+  if (number % 5 == 0)
+    result << "Buzz"
+  end
+  
+  if (result == "")
+    return nil
+  end
+  
+  return result
 }
